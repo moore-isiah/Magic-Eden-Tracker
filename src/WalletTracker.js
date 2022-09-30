57466650578fa503cc784d238825d23009b75813
Isiah Moore <isiahistj@gmail.com>
2022-09-30 13:00:30 -0600

    changing data fetch from sol scan to magic eden, adding new wallet submit button

--- src/WalletTracker.js
+++ src/WalletTracker.js
@@ -1,61 +1,33 @@
 import React, {useState, useEffect} from 'react'
 
 export default function WalletTracker(){
-    const [transactions, setTransactions] = useState()
-    const [wallet, setWallet] = useState('9C89G5Z151iZsJmu9evZzQ5z2R9vsGvrKidwcQSRRo3S')
-    const [secondFetch, setSecondFetch] = useState()
-    
-    useEffect(() => {
-        async function getData(){
-            /*
-            getting initial solTransfer data from sol scan api in batched of 50 per request
-            re-fetches data if the submitted wallet is changed
-            base url: https://public-api.solscan.io/
-            */
-            let returnAmount
-            let finalData = []
-            let i = 0
-            do{
-                const res = await fetch(`https://public-api.solscan.io/account/solTransfers?account=${wallet}&limit=50&offset=${i * 50}`)
-                const data = await res.json()
-                finalData = [...finalData, ...data.data]
-                returnAmount = data.data.length
-                i++
-            }while(returnAmount == 50)
+    const [wallet, setWallet] = useState('9ZYCXH5L3znQ1fiLHpA4SaQ5NeScWgrCbFjQNoNjR8VW')
+    const [userInput, setUserInput] = useState('')
+    const [activity, setActivity] = useState()
 
-        let tempArr = finalData.map(item => item.txHash)
-        setSecondFetch(tempArr)
+    useEffect(() => {
+ 
+         async function getData() {
+            let i = 0
+            let tempData = []
+            let returnAmount
+            do{
+                const res = await fetch(`https://api-mainnet.magiceden.dev/v2/wallets/${wallet}/activities?offset=${i * 500}&limit=1`)
+                const data = await res.json()
+                tempData = [...tempData, ...data]
+                returnAmount = data.length
+                i++
+            }while(returnAmount == 500)
+            setActivity(tempData)
         }
         getData()
     }, [wallet])
 
-    useEffect(() => {
-        async function getMoreData() {
-            /* 
-            using the txHash: property (transactin signiture) to fetch individual transaction data because solTransfers does not provide the amount of sol transfered
-            */
-            let arr = []
-            let almost = []
-            for(let i = 0; i < secondFetch.length; i++) {
-                const res2 = await fetch(`https://public-api.solscan.io/transaction/${secondFetch[i]}`)
-                const data2 = await res2.json()
-                arr.push(data2.inputAccount)
-            }
-            console.log(arr)
-            for(let j = 0; j < arr.length; j++) {
-                let temp = arr[j].filter(item => item.account == wallet)
-                almost = [...almost, ...temp]
-            }
-            
-        }
-        getMoreData()
-    }, [secondFetch])
-
-
-
     return (
         <div>
-            working wallet tracker
+            <input type='textbox' value={userInput} onChange={(event) => setUserInput(event.target.value)} placeholder='New Solana Wallet'></input>
+            <button onClick={() => setWallet(userInput)}>Submit Wallet</button>
         </div>
     )
 }
+
