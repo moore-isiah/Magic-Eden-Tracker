08620114b74c03032ac64700ba71191fb9d7e041
Isiah Moore <isiahistj@gmail.com>
2022-10-06 09:31:53 -0600

    buys and sells with name and img only

--- src/Homepage.js
+++ src/Homepage.js
@@ -1,28 +1,11 @@
 import React from 'react'
 
-import WalletTracker from './WalletTracker'
-
 export default function Homepage(){
 
 
     return (
         <div className='homepage'>
-            <div className='homeComponent'>
-            <h1 className='title'></h1>
-            <WalletTracker />
-        </div>
-        <div className='homeComponent'>
-            <h1 className='title'></h1>
-            
-        </div>
-        <div className='homeComponent'>
-            <h1 className='title'></h1>
-            
-        </div>
-        <div className='homeComponent'>
-            <h1 className='title'></h1>
-            
-        </div>
+           
         </div>
     )
 }
--- src/WalletTracker.js
+++ src/WalletTracker.js
@@ -1,129 +1,80 @@
 import React, { useState, useEffect } from 'react'
 import { useInfiniteQuery } from 'react-query'
+import errorImg from './icons/errorImg.png'
 
 export default function WalletTracker(){
     const [wallet, setWallet] = useState('9ZYCXH5L3znQ1fiLHpA4SaQ5NeScWgrCbFjQNoNjR8VW')
     const [userInput, setUserInput] = useState('')
+    const [photos, setPhotos] = useState([])
 
-    async function getData(wal, page){
-        const walletRes = await fetch(`https://api-mainnet.magiceden.dev/v2/wallets/9ZYCXH5L3znQ1fiLHpA4SaQ5NeScWgrCbFjQNoNjR8VW/activities?offset=${page * 10}&limit=10`)
-        const walletData = await walletRes.json()
-        return walletData
-    }
-
-    const {
-        status,
-        data,
-        error,
-        isFetching,
-        isFetchingNextPage,
-        isFetchingPreviousPage,
-        fetchNextPage,
-        fetchPreviousPage,
-        hasNextPage,
-        hasPreviousPage,
-      } = useInfiniteQuery(['transactions'], async ({ pageParam = 1}) => {
-        const walletRes = await fetch(`https://api-mainnet.magiceden.dev/v2/wallets/9ZYCXH5L3znQ1fiLHpA4SaQ5NeScWgrCbFjQNoNjR8VW/activities?offset=${(pageParam - 1 ) * 10}&limit=10`)
-        const data1 = await walletRes.json()
-        console.log(pageParam)
-        return data1
+    const {data, fetchNextPage} = useInfiniteQuery(['transactions'], async ({ pageParam = 1}, wal = wallet) => {
+        const walletRes = await fetch(`https://api-mainnet.magiceden.dev/v2/wallets/${wal}/activities?offset=${(pageParam - 1 ) * 50}&limit=50`)
+        const data = await walletRes.json()
+        return data
       },
-      {getNextPageParam: (lastPage, page) => lastPage.length === 10 ? page.length + 1 : undefined})
+      {getNextPageParam: (lastPage, page) => lastPage.length === 50 ? page.length + 1 : undefined})
+      useEffect(() => {
+        async function getPhotos(){
+            let tempArr = []
+            for(let i = 0; i < data.pages.length; i++){
+                for(let j = 0; j < data.pages[i].length; j++){
+                    const imgRes = await fetch(`https://api-mainnet.magiceden.dev/v2/tokens/${data.pages[i][j].tokenMint}`)
+                    const imgData = await imgRes.json()
+                    tempArr.push(imgData)
+                }
+            }
+            setPhotos(tempArr)
+        }
+        getPhotos()
+      }, [data])
 
-      console.log(data)
+      const buys = data?.pages.map(page => {
+        let temp = page.filter(arg => arg.type === 'buyNow' && arg.buyer === wallet)
+        let final = temp.map(item => {
+            let tempImg = photos.filter(img => img.mintAddress === item.tokenMint)
+            return (
+            <>    
+            <p>{item.collection}</p>
+            <img src={tempImg[0]?.image ? tempImg[0].image : errorImg} className='nft-img'/>
+            </>
+            )
+         })
+         return final
+     })
 
+     const sells = data?.pages.map(page => {
+        let temp = page.filter(arg => arg.type === 'buyNow' && arg.seller === wallet)
+        let final = temp.map(item => {
+            let tempImg = photos.filter(img => img.mintAddress === item.tokenMint)
+            return (
+            <>
+            <div className='scroll-item'>{item.collection}</div>
+            <img src={tempImg[0]?.image ? tempImg[0].image : errorImg} className='nft-img'/>
+            </>
+            )
+         })
+         return final
+     })
+
+
+    
     return (
         <> 
-                <input type='textbox' value={userInput} onChange={(event) => setUserInput(event.target.value)} placeholder='New Solana Wallet'></input>
+        <input type='textbox' value={userInput} onChange={(event) => setUserInput(event.target.value)} placeholder='New Solana Wallet'></input>
         <button onClick={() => setWallet(userInput)}>Submit Wallet</button>
         <div className='wallet-tracker'>
             
             <div className='containers'>
                 <p>Purchases</p>
                 <button onClick={() => fetchNextPage()}>get more</button>
-                
+                {buys}
             </div>
             <div className='containers'>
-                <p>listings</p>
+                <p>Sold</p>
+                {sells}
             </div>
             
         </div>
         </>
     )
 }
-
-/*
-
-const [activityData, setActivityData] = useState({
-    buys: [],
-    sells: [],
-    bidSent: [],
-    bidRecieve: [],
-    urls: {}
-})
-const [collections, setCollections] = useState()
-
-useEffect(() => {
-    async function getBuySell() {
-        let i = 0
-        let tempData = []
-        let returnAmount
-        
-        do{
-            const walletRes = await fetch(`https://api-mainnet.magiceden.dev/v2/wallets/${wallet}/activities?offset=${i * 500}&limit=500`)
-            const walletData = await walletRes.json()
-            tempData = [...tempData, ...walletData]
-            returnAmount = walletData.length
-            i++
-
-        }while(returnAmount === 500)
-        setActivityData({buys: tempData.filter(item => (item.buyer === wallet && item.type === 'buyNow')), sells: tempData.filter(item => (item.seller === wallet && item.type === 'buyNow')), bidSent: tempData.filter(item => (item.buyer === wallet && item.type === 'bid'))})
-
-        //console.log(tempData)
-    }
-    getBuySell()
-
-//        async function getBids(){
-//          let offerOut, offerIn
-//        const offerOutRes = await fetch(`https://api-mainnet.magiceden.dev/v2/wallets/${wallet}/offers_made?offset=${i * 500}&limit=500`)
-  //      const offerOutData = await offerOutRes.json()
-
-    //    console.log(offerOutData)
-      //  offerOut = offerOutData
-
-   // }
-    
-}, [wallet])
-
-
-//console.log(activityData.buys, activityData.sells, activityData.bidRecieve)
-
-useEffect(() => {
-    async function getCollections(){
-        let responseAmount
-        let arr = []
-        let i = 0
-        do{
-            const collectionRes = await fetch(`https://api-mainnet.magiceden.dev/v2/collections?offset=${i * 500}&limit=500`)
-            const collectionData = await collectionRes.json()
-            responseAmount = collectionData.length
-            arr = [...arr, ...collectionData]
-            i++
-        }while(responseAmount === 500)
-       // console.log(arr)
-
-        setCollections(arr)
-    }
-    getCollections()
-}, [activityData])
-
-
-// let buyItems = activityData.buys.map(act => collections.find(coll => {
-   // if(coll.symbol == act.collectionSymbol){
-      //  setActivityData({...activityData, urls: {...activityData.urls, [coll.symbol]: coll.image}})
-    //    return(
-  //          <p>{act.collectionSymbol}</p>
-//        )
-//      }
-//    }))
-*/
