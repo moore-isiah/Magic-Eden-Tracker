9281be94ee2fc55a10d5aaddc6ad4721af4e8066
Isiah Moore <isiahistj@gmail.com>
2023-05-29 22:25:25 -0600

    slight updates

--- src/Portfolio.js
+++ src/Portfolio.js
@@ -1,112 +1,145 @@
-import React, { useState } from 'react'
-import { useQuery } from 'react-query'
+import React, { useState } from "react";
+import { useQuery } from "react-query";
 
-import solLogo from './icons/solana-sol-logo.svg'
+import solLogo from "./icons/solana-sol-logo.svg";
 
-const decimal = .000000001
+const decimal = 0.000000001;
 
-export default function Portfolio(){
-    const [wallet, setWallet] = useState('9ZYCXH5L3znQ1fiLHpA4SaQ5NeScWgrCbFjQNoNjR8VW')
-    const [userInput, setUserInput] = useState('')
+export default function Portfolio() {
+  const [wallet, setWallet] = useState(
+    "AC1r4GYj1TZ62ASZS4VT6GnNtMzoD78wYNWfxJ3Uzbxy"
+  );
+  const [userInput, setUserInput] = useState("");
 
-    const {data, refetch: refetchCollections} = useQuery([`portfolio${wallet}`], 
+  const { data, refetch: refetchCollections } = useQuery(
+    [`portfolio${wallet}`],
     async ({}, wal = wallet) => {
-        let final = []
-        const res = await fetch(`https://api-mainnet.magiceden.dev/v2/wallets/${wal}/tokens`)
-        const data = await res.json()
-        let tempArr = []
+      let final = [];
+      const res = await fetch(
+        `https://api-mainnet.magiceden.dev/v2/wallets/${wal}/tokens`
+      );
+      const data = await res.json();
+      let tempArr = [];
 
-        for(let i = 0; i < data.length; i++){
-            if(data[i]?.collection !== data[i-1]?.collection){                
-                tempArr[0] ? final.push(tempArr) : console.log(i, 'empty')
-                tempArr = []
-                tempArr.push(data[i])
-            }else {
-                tempArr.push(data[i])
-            }
+      for (let i = 0; i < data.length; i++) {
+        if (data[i]?.collection !== data[i - 1]?.collection) {
+          tempArr[0] ? final.push(tempArr) : console.log(i, "empty");
+          tempArr = [];
+          tempArr.push(data[i]);
+        } else {
+          tempArr.push(data[i]);
         }
-        return final
+      }
+      return final;
     }
-    )
-    console.log(data)
-    const Collections = () => {
-        const CollectionHeader = (props) => {
-
-            const {data: stats} = useQuery([props.show.collection],
-                async ({}, name = props.show.collection) => {
-                    const statsRes = await fetch(`https://api-mainnet.magiceden.dev/v2/collections/${name}/stats`)
-                    const statsData = await statsRes.json()
-                    return statsData
-                }
-            )
-
-            const twoRound = (num) => {
-                return Math.round(num * 100)/100
-            }
-
-            let floor = stats?.floorPrice * decimal
-            let totalFloor = floor * props.amount
-            let meFees = .02
-            let royalty = props.show.sellerFeeBasisPoints * .0001
-            
-            return(
-                <div className='collection-header'>
-                    <div className='header-title'>
-                        <img className='collection-img' src={props.show.image} />
-                        <p>{props.show.collectionName}</p>
-                    </div>
-                    <p>Listed: {stats?.listedCount}</p>
-                    <p>Floor:<img className='port-sol' src={solLogo}/>{twoRound(floor)}</p>
-                    <div className='floor-container'>    
-                        <p>Total Floor Value:<img className='port-sol' src={solLogo}/>{twoRound(totalFloor)}</p>
-                        <p>With ME Fees:<img className='port-sol' src={solLogo}/>{twoRound(totalFloor - (totalFloor * meFees))}</p>
-                        <p>With Fees and Royalties:<img className='port-sol' src={solLogo}/>{twoRound(totalFloor - (totalFloor * (meFees + royalty)))}</p>
-                    </div>
-                </div>
-            )
+  );
+  console.log(data);
+  const Collections = () => {
+    const CollectionHeader = (props) => {
+      const { data: stats } = useQuery(
+        [props.show.collection],
+        async ({}, name = props.show.collection) => {
+          const statsRes = await fetch(
+            `https://api-mainnet.magiceden.dev/v2/collections/${name}/stats`
+          );
+          const statsData = await statsRes.json();
+          return statsData;
         }
+      );
 
-        return(
-        <div className='collections-containter'>
-            {data?.map(group => {
-               
-                return (
-                <div className='collection'>
-                    <CollectionHeader show={group[0]} amount={group.length}/>
-                    <div className='item-container'>
-                    {group.map(item =>{
-                        console.log(item)
-                        return(
-                            <div className='collection-item'>
-                                <img className='item-img' src={item.image} />
-                                <p className='item-name'>{item.name}</p>
-                            </div>
-                        )
-                    })
-                    }
-                    </div>
-                </div>
-                )
-            })}
+      const twoRound = (num) => {
+        return Math.round(num * 100) / 100;
+      };
+
+      let floor = stats?.floorPrice * decimal;
+      let totalFloor = floor * props.amount;
+      let meFees = 0.02;
+      let royalty = props.show.sellerFeeBasisPoints * 0.0001;
+
+      return (
+        <div className="collection-header">
+          <div className="header-title">
+            <img className="collection-img" src={props.show.image} />
+            <p>{props.show.collectionName}</p>
+          </div>
+          <p>Listed: {stats?.listedCount}</p>
+          <p>
+            Floor:
+            <img className="port-sol" src={solLogo} />
+            {twoRound(floor)}
+          </p>
+          <div className="floor-container">
+            <p>
+              Total Floor Value:
+              <img className="port-sol" src={solLogo} />
+              {twoRound(totalFloor)}
+            </p>
+            <p>
+              With ME Fees:
+              <img className="port-sol" src={solLogo} />
+              {twoRound(totalFloor - totalFloor * meFees)}
+            </p>
+            <p>
+              With Fees and Royalties:
+              <img className="port-sol" src={solLogo} />
+              {twoRound(totalFloor - totalFloor * (meFees + royalty))}
+            </p>
+          </div>
         </div>
-        )
-        }
+      );
+    };
 
-    async function newWallet() {
-        const res = await fetch(`https://api-mainnet.magiceden.dev/v2/wallets/${userInput}/tokens`)
-
-        await res.ok ? setWallet(userInput) : window.alert('Please enter a valid wallet!!!')
-
-        refetchCollections()
-    }
-
-    return(
-        <div className='portfolio'>
-            <div className='port-enter-section' >
-                <input className='port-input' type='text' onChange={(e) => setUserInput(e.target.value)} value={userInput} placeholder='Solana Wallet' />
-                <button className='port-button' onClick={newWallet}>Submit</button>
+    return (
+      <div className="collections-containter">
+        {data?.map((group) => {
+          return (
+            <div className="collection">
+              <CollectionHeader show={group[0]} amount={group.length} />
+              <div className="item-container">
+                {group.map((item) => {
+                  console.log(item);
+                  return (
+                    <div className="collection-item">
+                      <img className="item-img" src={item.image} />
+                      <p className="item-name">{item.name}</p>
+                    </div>
+                  );
+                })}
+              </div>
             </div>
-            <Collections/> 
-        </div>
-    )
+          );
+        })}
+      </div>
+    );
+  };
+
+  async function newWallet() {
+    const res = await fetch(
+      `https://api-mainnet.magiceden.dev/v2/wallets/${userInput}/tokens`
+    );
+
+    (await res.ok)
+      ? setWallet(userInput)
+      : window.alert("Please enter a valid wallet!!!");
+
+    refetchCollections();
+  }
+
+  return (
+    <div className="portfolio">
+      <div className="port-enter-section">
+        <input
+          className="port-input"
+          type="text"
+          onChange={(e) => setUserInput(e.target.value)}
+          value={userInput}
+          placeholder="Solana Wallet"
+        />
+        <button className="port-button" onClick={newWallet}>
+          Submit
+        </button>
+      </div>
+      <Collections />
+    </div>
+  );
 }
--- src/WalletTracker.js
+++ src/WalletTracker.js
@@ -1,198 +1,247 @@
-import React, { useState, useEffect } from 'react'
-import { useInfiniteQuery, useQuery } from 'react-query'
-import errorImg from './icons/errorImg.png'
-import solLogo from './icons/solana-sol-logo.svg'
+import React, { useState, useEffect } from "react";
+import { useInfiniteQuery, useQuery } from "react-query";
+import errorImg from "./icons/errorImg.png";
+import solLogo from "./icons/solana-sol-logo.svg";
 
-//track the buys, sells, sent bids and recieved bids of a specified solana wallet on Magic Eden (ME), all fetches are to different ME end points 
+//track the buys, sells, sent bids and recieved bids of a specified solana wallet on Magic Eden (ME), all fetches are to different ME end points
 const WalletTracker = () => {
-    const [wallet, setWallet] = useState('9ZYCXH5L3znQ1fiLHpA4SaQ5NeScWgrCbFjQNoNjR8VW')
-    const [userInput, setUserInput] = useState('')
+  const [wallet, setWallet] = useState(
+    "AC1r4GYj1TZ62ASZS4VT6GnNtMzoD78wYNWfxJ3Uzbxy"
+  );
+  const [userInput, setUserInput] = useState("");
 
-    //using react-query to fetching all transactions from ME
-    const {data: allData, fetchNextPage, refetch: refetchTransactions} = useInfiniteQuery(['transactions'], async ({ pageParam = 1}, wal = wallet) => {
-        const walletRes = await fetch(`https://api-mainnet.magiceden.dev/v2/wallets/${wal}/activities?offset=${(pageParam - 1 ) * 50}&limit=50`)
-        const data = await walletRes.json()
-        return data
+  //using react-query to fetching all transactions from ME
+  const {
+    data: allData,
+    fetchNextPage,
+    refetch: refetchTransactions,
+  } = useInfiniteQuery(
+    ["transactions"],
+    async ({ pageParam = 1 }, wal = wallet) => {
+      const walletRes = await fetch(
+        `https://api-mainnet.magiceden.dev/v2/wallets/${wal}/activities?offset=${
+          (pageParam - 1) * 50
+        }&limit=50`
+      );
+      const data = await walletRes.json();
+      return data;
     },
-    {getNextPageParam: (lastPage, page) => lastPage.length === 50 ? page.length + 1 : undefined,
-    })
-
-    //using react-query fetch all of the sent offers
-    const {data: bidOutData, refetch: refetchBidsOut} = useQuery(['bidOutData'], async ({}, wal = wallet) => {
-        const bidOutRes = await fetch(`https://api-mainnet.magiceden.dev/v2/wallets/${wal}/offers_made`)
-        const bidOutD = await bidOutRes.json()
-        return bidOutD
-    })
-
-    //using react-query to fetch all bids revieved
-    const {data: bidInData, refetch: refetchBidsIn} = useQuery(['bidInData'], async ({}, wal = wallet) => {
-        const bidInRes = await fetch(`https://api-mainnet.magiceden.dev/v2/wallets/${wal}/offers_received`)
-        const bidInD = await bidInRes.json()
-        return bidInD
-    })
-
-    //once the user inputs a new wallet set the wallet state to the new one and wait a second for it to update then refetch everything above
-    async function newWallet() {
-        const res = await fetch(`https://api-mainnet.magiceden.dev/v2/wallets/${userInput}/tokens`)
-
-        await res.ok ? setWallet(userInput) : window.alert('Please enter a valid wallet!!!')
-        
-
-        refetchTransactions()
-        refetchBidsIn()
-        refetchBidsOut()
+    {
+      getNextPageParam: (lastPage, page) =>
+        lastPage.length === 50 ? page.length + 1 : undefined,
     }
+  );
 
-    const handleScroll = (e) => {
-        const bottom = e.target.scrollHeight - e.target.scrollTop - e.target.clientHeight < 1;
-        if(bottom){
-            fetchNextPage()
-        }
+  //using react-query fetch all of the sent offers
+  const { data: bidOutData, refetch: refetchBidsOut } = useQuery(
+    ["bidOutData"],
+    async ({}, wal = wallet) => {
+      const bidOutRes = await fetch(
+        `https://api-mainnet.magiceden.dev/v2/wallets/${wal}/offers_made`
+      );
+      const bidOutD = await bidOutRes.json();
+      return bidOutD;
     }
+  );
 
-    const ScrollItem = (props) =>{
-        const {data, isLoading, isError} = useQuery([props.tokenMint], async ({}, token = props.tokenMint) => {
-            const imgRes = await fetch(`https://api-mainnet.magiceden.dev/v2/tokens/${token}`)
-            const imgData = await imgRes.json()
-            return imgData
-        }, {staleTime: Infinity})
-        if(isLoading){
-            <div className='scroll-item'>
-                <div className='scroll-info'>Loading...</div>
+  //using react-query to fetch all bids revieved
+  const { data: bidInData, refetch: refetchBidsIn } = useQuery(
+    ["bidInData"],
+    async ({}, wal = wallet) => {
+      const bidInRes = await fetch(
+        `https://api-mainnet.magiceden.dev/v2/wallets/${wal}/offers_received`
+      );
+      const bidInD = await bidInRes.json();
+      return bidInD;
+    }
+  );
+
+  //once the user inputs a new wallet set the wallet state to the new one and wait a second for it to update then refetch everything above
+  async function newWallet() {
+    const res = await fetch(
+      `https://api-mainnet.magiceden.dev/v2/wallets/${userInput}/tokens`
+    );
+
+    (await res.ok)
+      ? setWallet(userInput)
+      : window.alert("Please enter a valid wallet!!!");
+
+    refetchTransactions();
+    refetchBidsIn();
+    refetchBidsOut();
+  }
+
+  const handleScroll = (e) => {
+    const bottom =
+      e.target.scrollHeight - e.target.scrollTop - e.target.clientHeight < 1;
+    if (bottom) {
+      fetchNextPage();
+    }
+  };
+
+  const ScrollItem = (props) => {
+    const { data, isLoading, isError } = useQuery(
+      [props.tokenMint],
+      async ({}, token = props.tokenMint) => {
+        const imgRes = await fetch(
+          `https://api-mainnet.magiceden.dev/v2/tokens/${token}`
+        );
+        const imgData = await imgRes.json();
+        return imgData;
+      },
+      { staleTime: Infinity }
+    );
+    if (isLoading) {
+      <div className="scroll-item">
+        <div className="scroll-info">Loading...</div>
+      </div>;
+    } else if (isError) {
+      <div className="scroll-item">
+        <div className="scroll-info">Error Fetching Data</div>
+      </div>;
+    } else {
+      if (data.image) {
+        return (
+          <div className="scroll-item">
+            <div className="scroll-info">
+              <div>{data.name}</div>
+              <img src={solLogo} className="sol-logo" />
+              <div>{props.price}</div>
+              {props.type ? <div className="type">{props.type}</div> : null}
             </div>
-        }else if(isError){
-            <div className='scroll-item'>
-                <div className='scroll-info'>Error Fetching Data</div>
+            <img src={data.image} className="scroll-img" />
+          </div>
+        );
+      } else {
+        return (
+          <div className="scroll-item">
+            <div className="scroll-info">
+              <div>Error Fetching Meta Data</div>
             </div>
-        }else{
-            if(data.image){
-                return (
-                    <div className='scroll-item'>
-                        <div className='scroll-info'>
-                            <div>{data.name}</div>
-                            <img src={solLogo} className='sol-logo'/>
-                            <div>{props.price}</div>
-                            {props.type ? (<div className='type'>{props.type}</div>) : null}
-                        </div>
-                        <img src={data.image} className='scroll-img'/>
-                    </div>
-                )
-           }else{
+            <img src={errorImg} className="scroll-img" />
+          </div>
+        );
+      }
+    }
+  };
+
+  //maping through the data, filtering the buys of the provided wallet, then filtering the photos state to find its matching image adn returning the jsx to be displayed
+
+  const buys = (
+    <div className="containers" onScroll={handleScroll}>
+      <div className="container-label">
+        <p>Purchases</p>
+      </div>
+      <div className="scroll-container">
+        {allData?.pages.map((page) => {
+          let temp = page?.filter(
+            (arg) => arg.type === "buyNow" && arg.buyer === wallet
+          );
+          let final = temp.map((item) => {
+            return <ScrollItem tokenMint={item.tokenMint} price={item.price} />;
+          });
+          return final;
+        })}
+      </div>
+    </div>
+  );
+
+  //same as the previous but for the sells
+  const sells = (
+    <div className="containers" onScroll={handleScroll}>
+      <div className="container-label">
+        <p>Sales</p>
+      </div>
+      <div className="scroll-container">
+        {allData?.pages.map((page) => {
+          let temp = page.filter(
+            (arg) => arg.type === "buyNow" && arg.seller === wallet
+          );
+          let final = temp.map((item) => {
+            return <ScrollItem tokenMint={item.tokenMint} price={item.price} />;
+          });
+          return final;
+        })}
+      </div>
+    </div>
+  );
+
+  const allActivity = (
+    <div className="containers all-activity" onScroll={handleScroll}>
+      <div className="container-label all-label">
+        <p>All Activity</p>
+      </div>
+      <div className="scroll-container">
+        {allData?.pages.map((page) => {
+          let final = page.map((item) => {
             return (
-                <div className='scroll-item'>
-                    <div className='scroll-info'>
-                        <div>Error Fetching Meta Data</div>
-                    </div>
-                    <img src={errorImg} className='scroll-img'/>
-                </div>
-            
-            )
-           }
-        }
-    }
+              <ScrollItem
+                tokenMint={item.tokenMint}
+                price={item.price}
+                type={item.type}
+              />
+            );
+          });
+          return final;
+        })}
+      </div>
+    </div>
+  );
 
-    //maping through the data, filtering the buys of the provided wallet, then filtering the photos state to find its matching image adn returning the jsx to be displayed
-    
-    const buys = (
-        <div className='containers' onScroll={handleScroll}>
-            <div className='container-label'>
-                <p>Purchases</p>
-            </div>
-            <div className='scroll-container'>
-                {
-                    allData?.pages.map(page => {
-                        let temp = page?.filter(arg => arg.type === 'buyNow' && arg.buyer === wallet)
-                        let final = temp.map(item => {
-                            return <ScrollItem tokenMint={item.tokenMint} price={item.price} />
-                        })
-                        return final
-                    })
-                }
-            </div>
+  //same as the previous two but for sent bids
+  const bidsOut = bidOutData?.map((item) => (
+    <ScrollItem tokenMint={item.tokenMint} />
+  ));
+
+  //same as the previous 3 but for recieved bids
+  const bidsIn = bidInData?.map((item) => (
+    <ScrollItem tokenMint={item.tokenMint} />
+  ));
+
+  return (
+    <div className="wallet-tracker">
+      {buys}
+
+      <div className="containers submit-section">
+        <input
+          type="textbox"
+          className="wallet-textbox"
+          value={userInput}
+          onChange={(event) => setUserInput(event.target.value)}
+          placeholder="Enter New Solana Wallet"
+        />
+        <button className="submit-wallet-button" onClick={() => newWallet()}>
+          Submit Wallet
+        </button>
+        <p>
+          Use the default wallet to test the website or submit your own and
+          track your own activity.
+        </p>
+      </div>
+
+      {sells}
+
+      <div className="containers">
+        <div className="container-label">
+          <p>Sent Offers</p>
         </div>
-    )
-
-    //same as the previous but for the sells
-    const sells = (
-        <div className='containers' onScroll={handleScroll}>
-            <div className='container-label'>
-                <p>Sales</p>
-            </div>
-            <div className='scroll-container'>
-                {
-                    allData?.pages.map(page => {
-                        let temp = page.filter(arg => arg.type === 'buyNow' && arg.seller === wallet)
-                        let final = temp.map(item => {
-                            return <ScrollItem tokenMint={item.tokenMint} price={item.price} />
-                        })
-                        return final
-                    })
-                }
-            </div>
-        </div> 
-    )
-
-    const allActivity =  (
-        <div className='containers all-activity' onScroll={handleScroll}>
-                <div className='container-label all-label'>
-                    <p>All Activity</p>
-                </div>
-                <div className='scroll-container'>
-                    {
-                        allData?.pages.map(page => {
-                            let final = page.map(item => {
-                                return <ScrollItem tokenMint={item.tokenMint} price={item.price} type={item.type}/>
-                            })
-                            return final
-                        })
-                    }
-                </div>
-            </div>
-        
-    )
-
-    //same as the previous two but for sent bids
-    const bidsOut = bidOutData?.map(item => <ScrollItem tokenMint={item.tokenMint} />)
-
-    //same as the previous 3 but for recieved bids
-    const bidsIn = bidInData?.map(item => <ScrollItem tokenMint={item.tokenMint} />)
-
-    return (
-
-        <div className='wallet-tracker'>
-
-            {buys}
-
-            <div className='containers submit-section'>
-                <input type='textbox' className='wallet-textbox' value={userInput} onChange={(event) => setUserInput(event.target.value)} placeholder='Enter New Solana Wallet' />
-                <button className='submit-wallet-button' onClick={() => newWallet()}>Submit Wallet</button>
-                <p>Use the default wallet to test the website or submit your own and track your own activity.</p>
-            </div>
-
-           {sells}
-
-            <div className='containers'>
-                <div className='container-label'>
-                    <p>Sent Offers</p>
-                </div>
-                <div className='scroll-container'>
-                    {bidsOut?.length === 0 ? 'No Bids to Display' : bidsOut}
-                </div>
-            </div>
-
-            {allActivity}
-
-            <div className='containers'>
-                <div className='container-label'>
-                    <p>Offers Recieved</p>
-                </div>
-                <div className='scroll-container'>
-                    {bidsIn?.length === 0 ? 'No Offers to Display' : bidsIn}
-                </div>
-            </div>
-            
+        <div className="scroll-container">
+          {bidsOut?.length === 0 ? "No Bids to Display" : bidsOut}
         </div>
-    )
-}
+      </div>
 
-export default WalletTracker
+      {allActivity}
+
+      <div className="containers">
+        <div className="container-label">
+          <p>Offers Recieved</p>
+        </div>
+        <div className="scroll-container">
+          {bidsIn?.length === 0 ? "No Offers to Display" : bidsIn}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default WalletTracker;
